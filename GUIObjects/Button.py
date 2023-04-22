777b992ff351f3bc7bf6ba5ba4235e673fd34754
dydgks789@naver.com <dydgks789@naver.com>
2023-04-22 13:40:55 +0900

    버튼 별 Action 구현

--- GUIObjects/Button.py
+++ GUIObjects/Button.py
@@ -1,4 +1,7 @@
+import random
 import pygame
+from utils import generate_start_goal, set_randomly_obstacles, reset_obstacles
+from AstartAlgorithm.Astar import Astar
 
 class Button:
 
@@ -18,3 +21,25 @@ class Button:
         button_text = self.font.render(self.text, True, self.text_color)
         text_rect = button_text.get_rect(center=(self.x + self.width // 2, self.y + self.height // 2))
         screen.blit(button_text, text_rect)
+
+    # A* 알고리즘 실행
+    def start_Astar_search(self, grid_world, start, end, heuristic_function):
+        model = Astar(grid_world, start, end, heuristic_function)
+        path, explored_nodes = model.get_path()
+        return path, explored_nodes
+
+    # 장애물 랜덤 배치
+    def set_randomly_obstacles(self, grid_world, num_obstacles):
+        rows, cols = len(grid_world), len(grid_world[0])
+        for _ in range(num_obstacles):
+            while True:
+                row, col = random.randint(0, rows - 1), random.randint(0, cols - 1)
+                if grid_world[row][col] != 'S' and grid_world[row][col] != 'G' and grid_world[row][col] != 'X':
+                    grid_world[row][col] = 'X'
+                    break
+
+    # 리셋
+    def reset_grid_world(self, M, N, inc_obstacle_ratio, grid_world):
+        reset_obstacle(grid_world)
+        start, end = generate_start_goal(M, N, grid_world)
+        return start, end
