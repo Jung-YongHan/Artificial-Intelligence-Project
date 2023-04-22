f83a4b1fa96997c9db6ef9e422a3090558fa7cb7
dydgks789@naver.com <dydgks789@naver.com>
2023-04-22 20:12:53 +0900

    구현 완료

--- GridWorld.py
+++ GridWorld.py
@@ -1,5 +1,9 @@
 import argparse
 import pygame
+import random
+from utils import *
+import time
+
 from GUIObjects.RadioButton import RadioButton
 from GUIObjects.Button import Button
 from GUIObjects.TextBox import TextBox
@@ -12,7 +16,10 @@ grid_world_size = (600, 600)
 black = (0, 0, 0)
 white = (255, 255, 255)
 gray = (220, 220, 220)
+dark_gray = (128, 128, 128)
 light_blue = (75, 137, 220)
+blue = (0, 0, 255)
+red = (255, 0, 0)
 
 # 사용할 폰트
 pygame.font.init()
@@ -69,7 +76,15 @@ text_box = TextBox(text_box_text, text_box_x, text_box_y, text_box_width, text_b
 
 # 라디오 버튼 정의
 radio_buttons = [RadioButton(radio_button_text[i], radio_button_x, radio_button_y+20*i+15, radio_button_width,
-                             font2, black, black, radio_button_radius) for i in range(2)]
+                             font2, black, black, radio_button_radius, i) for i in range(2)]
+
+def selected_heuristic():
+    for button in radio_buttons:
+        if button.selected:
+            if button.text == "Manhattan":
+                return 0
+            elif button.text == "Euclidean":
+                return 1
 
 def draw_world(M, N, inc_obstacle_ratio):
     pygame.init()
@@ -78,6 +93,9 @@ def draw_world(M, N, inc_obstacle_ratio):
 
     pygame.display.set_caption("Find a shortest path with A* Algorithm!")
 
+    grid_world = [['O' for _ in range(N)] for _ in range(M)]
+    ###### 그리드 만들어서 장애물, 시작, 도착지점 구현하기
+
     # 그리드 셀 크기 계산
     cell_width = grid_world_size[0] // M
     cell_height = grid_world_size[1] // N
@@ -85,23 +103,30 @@ def draw_world(M, N, inc_obstacle_ratio):
     # 라디오 버튼 기본 설정
     radio_buttons[0].selected = True
 
-    # Main loop
+    # 드래그 상태 기본 설정
+    drag_start = False
+    drag_goal = False
+
+    # 시작, 도착 위치
+    start, goal = generate_start_goal(M, N, grid_world)
+
+    # 시작, 도착값 변환을 위한 변수
+    prev_start = start
+    prev_goal = goal
+
+    # 경로 및 탐색 노드 개수
+    path = []
+    explored_node = 0
+
+    # 선이 그려진 경우, 경로 알고리즘 실행한 경우
+    lines = []
+    line_finished = False
+    astar_start = False
+    print_result = None
+
+    # game loop
     running = True
     while running:
-        for event in pygame.event.get():
-            # x 버튼 클릭 시 윈도우 종료
-            if event.type == pygame.QUIT:
-                running = False
-
-            # 마우스 클릭 이벤트
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                # 왼쪾 마우스 클릭
-                if event.button == 1:
-                    for button in radio_buttons:
-                        if button.is_clicked(event.pos):
-                            for other_button in radio_buttons:
-                                other_button.selected = False
-                            button.selected = True
 
         screen.fill(white)
 
@@ -117,9 +142,158 @@ def draw_world(M, N, inc_obstacle_ratio):
         # Grid World
         for row in range(M):
             for col in range(N):
-                pygame.draw.rect(screen, black, (75 + (col * cell_width), 100 + (row * cell_height), cell_width, cell_height), 1)
+                pygame.draw.rect(screen, black,
+                                 (75 + (col * cell_width), 100 + (row * cell_height), cell_width, cell_height), 1)
 
-        # 이중 버퍼 활용, 화면 전환
+        # 장애물 개수
+        obstacle_count_text = font1.render(f"Obstacles: {count_obstacles(grid_world)}", True, black)
+        screen.blit(obstacle_count_text, (window_size[0] - 150, window_size[1] - 35))
+
+        # 시작, 도착
+        start_rect = pygame.draw.rect(screen, blue,
+                                      (75 + start[1] * cell_width, 100 + start[0] * cell_height, cell_width,
+                                       cell_height))
+        goal_rect = pygame.draw.rect(screen, red,
+                                     (75 + goal[1] * cell_width, 100 + goal[0] * cell_height, cell_width, cell_height))
+
+
+        # 장애물
+        for row in range(M):
+            for col in range(N):
+                if grid_world[row][col] == 'X':
+                    pygame.draw.rect(screen, dark_gray,
+                                     (75 + (col * cell_width), 100 + (row * cell_height), cell_width, cell_height))
+                    pygame.draw.rect(screen, black,
+                                     (75 + (col * cell_width), 100 + (row * cell_height), cell_width, cell_height), 1)
+                elif grid_world[row][col] == 'O':
+                    pygame.draw.rect(screen, black,
+                                     (75 + (col * cell_width), 100 + (row * cell_height), cell_width, cell_height), 1)
+
+        if not line_finished and astar_start:
+            lines = []
+            for i in range(len(path) - 1):
+                lines.append(((75 + path[i][1] * cell_width + cell_width // 2,
+                               100 + path[i][0] * cell_height + cell_height // 2),
+                              (75 + path[i + 1][1] * cell_width + cell_width // 2,
+                               100 + path[i + 1][0] * cell_height + cell_height // 2)))
+            for line_start, line_end in lines:
+                pygame.draw.line(screen, (255, 255, 0), line_start, line_end, 5)
+                pygame.time.delay(50)
+                pygame.display.flip()
+            line_finished = True
+
+        if line_finished and astar_start:
+            for line_start, line_end in lines:
+                pygame.draw.line(screen, (255, 255, 0), line_start, line_end, 5)
+            if not print_result:
+                print_result = True
+                if path[-1] == goal:
+                    print("목적지까지의 최단경로 분석을 완료했습니다!")
+                    print(f"탐색한 노드의 수 : {explored_node}")
+                else:
+                    print("목적지까지의 경로가 존재하지 않습니다!")
+                    print(f"탐색한 노드의 수 : {explored_node}")
+
+
+        for event in pygame.event.get():
+            # x 버튼 클릭 시 윈도우 종료
+            if event.type == pygame.QUIT:
+                running = False
+
+            # 마우스 클릭 이벤트
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # 왼쪽 마우스 클릭
+                if event.button == 1:
+
+                    # 버튼 Action
+                    # 선이 그려진 후 누를 수 있는 버튼은 경로 탐색, 리셋 버튼만 가능
+                    if line_finished:
+
+                        if basic_buttons[0].is_clicked(event.pos):
+                            path, explored_node = \
+                                basic_buttons[0].start_Astar_search(grid_world, start, goal, selected_heuristic())
+                            astar_start = True
+                            line_finished = False
+                            print_result = False
+                        if basic_buttons[2].is_clicked(event.pos):
+                            astar_start = False
+                            line_finished = False
+                            print_result = False
+                            basic_buttons[2].reset_grid_world(M, N, inc_obstacle_ratio, grid_world)
+
+                        # 라디오 버튼 Action
+                        for button in radio_buttons:
+                            if button.is_clicked(event.pos):
+                                for other_button in radio_buttons:
+                                    other_button.selected = False
+                                button.selected = True
+                    else:
+                        if basic_buttons[0].is_clicked(event.pos):
+                            path, explored_node = \
+                                basic_buttons[0].start_Astar_search(grid_world, start, goal, selected_heuristic())
+                            astar_start = True
+                        if basic_buttons[1].is_clicked(event.pos):
+                            astar_start = False
+                            basic_buttons[1].set_randomly_obstacles(grid_world, int(M*N*inc_obstacle_ratio))
+                        if basic_buttons[2].is_clicked(event.pos):
+                            astar_start = False
+                            basic_buttons[2].reset_grid_world(M, N, inc_obstacle_ratio, grid_world)
+
+                        # 라디오 버튼 Action
+                        for button in radio_buttons:
+                            if button.is_clicked(event.pos):
+                                for other_button in radio_buttons:
+                                    other_button.selected = False
+                                button.selected = True
+
+                        # 드래그 Action
+                        pos = pygame.mouse.get_pos()
+                        if start_rect.collidepoint(pos):
+                            drag_start = True
+                            offset_x, offset_y = start_rect.x - pos[0], start_rect.y - pos[1]
+                        if goal_rect.collidepoint(pos):
+                            drag_goal = True
+                            offset_x, offset_y = goal_rect.x - pos[0], goal_rect.y - pos[1]
+
+                        # 장애물 Action
+                        if is_valid(event.pos[0], event.pos[1], cell_width, cell_height, grid_world):
+                            toggle_obstacle(pos, cell_width, cell_height, grid_world)
+
+
+            if event.type == pygame.MOUSEBUTTONUP:
+                drag_start = False
+                drag_goal = False
+
+            if event.type == pygame.MOUSEMOTION:
+                if drag_start:
+                    new_x = event.pos[0] + offset_x
+                    new_y = event.pos[1] + offset_y
+                    if is_valid(new_x, new_y, cell_width, cell_height, grid_world):
+                        grid_world[prev_start[0]][prev_start[1]] = 'O'
+
+                        start_rect.x = new_x
+                        start_rect.y = new_y
+                        start = (start_rect.y - 100) // cell_height, (start_rect.x - 75) // cell_width
+
+                        grid_world[start[0]][start[1]] = 'S'
+                        prev_start = start
+
+                if drag_goal:
+                    new_x = event.pos[0] + offset_x
+                    new_y = event.pos[1] + offset_y
+                    if is_valid(new_x, new_y, cell_width, cell_height, grid_world):
+                        grid_world[prev_goal[0]][prev_goal[1]] = 'O'
+
+                        goal_rect.x = new_x
+                        goal_rect.y = new_y
+                        goal = (goal_rect.y - 100) // cell_height, (goal_rect.x - 75) // cell_width
+
+                        grid_world[goal[0]][goal[1]] = 'G'
+                        prev_goal = goal
+
+            # 이중 버퍼 활용, 화면 전환
         pygame.display.flip()
+
+
 
     pygame.quit()
--- main.py
+++ main.py
@@ -1,6 +1,33 @@
 import argparse
 from GridWorld import draw_world
 
+##################################예외처리 구현 필요
+# global window_size, grid_world_size
+# wind
+# grid_world_size = (600, 600)
+# min_cell_size = 10
+#
+# max_rows = grid_world_size[0] // min_cell_size
+# max_cols = grid_world_size[1] // min_cell_size
+#
+# def limited_int(x, max_value):
+#     x = int(x)
+#     if x < 1 or x > max_value:
+#         raise argparse.ArgumentTypeError(f"Value must be between 1 and {max_value}, but got {x}")
+#     return x
+#
+# def limited_rows(x):
+#     return limited_int(x, max_rows)
+#
+# def limited_cols(x):
+#     return limited_int(x, max_cols)
+#
+# def limited_float(x, max_value):
+#     if x < 0 or x > 0.95:
+#         raise argparse.ArgumentTypeError(f"Value must be between 0 and 0.95, but got {x}")
+#     return x
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="A* Algorithm Start!")
     parser.add_argument("--M", type=int, default=30, help="Number of rows")
