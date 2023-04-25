e94703f47f7c6f6e8d3dbfa13d1e85772316bb4d
dydgks789@naver.com <dydgks789@naver.com>
2023-04-25 20:06:13 +0900

    add argparse exception

--- main.py
+++ main.py
@@ -1,38 +1,23 @@
 import argparse
 from GridWorld import draw_world
 
-################################## need to implement Exceptions
-# global window_size, grid_world_size
-# wind
-# grid_world_size = (600, 600)
-# min_cell_size = 10
-#
-# max_rows = grid_world_size[0] // min_cell_size
-# max_cols = grid_world_size[1] // min_cell_size
-#
-# def limited_int(x, max_value):
-#     x = int(x)
-#     if x < 1 or x > max_value:
-#         raise argparse.ArgumentTypeError(f"Value must be between 1 and {max_value}, but got {x}")
-#     return x
-#
-# def limited_rows(x):
-#     return limited_int(x, max_rows)
-#
-# def limited_cols(x):
-#     return limited_int(x, max_cols)
-#
-# def limited_float(x, max_value):
-#     if x < 0 or x > 0.95:
-#         raise argparse.ArgumentTypeError(f"Value must be between 0 and 0.95, but got {x}")
-#     return x
+def check_mn(value):
+    ivalue = int(value)
+    if ivalue < 10 or ivalue > 60:
+        raise argparse.ArgumentTypeError("M and N must be between 2 and 60")
+    return ivalue
 
+def check_inc_obstacle_ratio(value):
+    fvalue = float(value)
+    if fvalue <= 0 or fvalue > 0.95:
+        raise argparse.ArgumentTypeError("inc_obstacle_ratio must be between 0 and 0.95 (exclusive)")
+    return fvalue
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="A* Algorithm Start!")
-    parser.add_argument("--M", type=int, default=30, help="Number of rows")
-    parser.add_argument("--N", type=int, default=30, help="Number of columns")
-    parser.add_argument("--inc_obstacle_ratio", type=float, default=0.2, help="Ratio of obstacle")
+    parser.add_argument("--M", type=check_mn, default=30, help="Number of rows")
+    parser.add_argument("--N", type=check_mn, default=30, help="Number of columns")
+    parser.add_argument("--inc_obstacle_ratio", type=check_inc_obstacle_ratio, default=0.2, help="Ratio of obstacle")
     args = parser.parse_args()
 
     draw_world(args.M, args.N, args.inc_obstacle_ratio)
